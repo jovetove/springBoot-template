62d89d216d013679336333148fec620737751e83
JoveTove <32268828+jovetove@users.noreply.github.com>
2021-06-08 15:33:11 +0800

    整合了Swagger, 用于后台接口文档自动生成

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,22 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author jove
  */
 @RestController
+/** swaggerAPI @Api: 描述类/接口的主要用途 */
+@Api(value = "系统首页", tags="首页模块")
 public class MainController {
 
-    @RequestMapping("/")
+    /** @ApiOperation: 描述方法用途 */
+    @ApiOperation(value = "获取用户列表", notes = "获取所有用户信息")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello(){
         return "hello";
     }
